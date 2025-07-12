a5f01ec07db3f3cea9e686db72a877b09b5a391f
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-12 12:25:07 +0300

    maade migrations

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.conf import settings
 
+# class Tag(models.Model):
+#     name = models.CharField(max_length=30, unique=True)
+
+#     def __str__(self):
+#         return self.name
 class Tag(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
