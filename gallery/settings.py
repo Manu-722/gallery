bd84637270d39e1580bdafb7273b8dea9cc5a87d
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-12 22:43:11 +0300

    commented out duplicates

--- gallery/settings.py
+++ gallery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f3cp=xi95r859i(gseycc2h5un+1=xoawrb2c4+tdpcm5!v9ro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
