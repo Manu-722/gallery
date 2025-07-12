4c3eb3c915245c81080d3c6784053425edf9db04
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-12 22:47:07 +0300

    commented out duplicates

--- gallery/urls.py
+++ gallery/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
